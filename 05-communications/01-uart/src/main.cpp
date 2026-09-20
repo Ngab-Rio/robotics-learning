37859d9dbf23b5767688528fbda0e2bb687f4be9
Ngab-Rio <mamanadifirmansyah@gmail.com>
2026-09-20 15:09:56 +0700

    add: 05 UART Communication

---
+++ 05-communications/01-uart/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define LED_PIN 2
+
+String message;
+bool ledState = false;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
+    Serial.print("LED Controller ");
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        message = Serial.readStringUntil('\n');
+        message.trim();
+        if (message == "on" || message == "ON") {
+            digitalWrite(LED_PIN, HIGH);
+            ledState = true;
+            Serial.println("-> LED Menyala");
+        } else if (message == "off" || message == "OFF") {
+            digitalWrite(LED_PIN, LOW);
+            ledState = false;
+            Serial.println("-> LED Mati");
+        } else if (message == "status" || message == "STATUS") {
+            Serial.print("-> LED ");
+            Serial.println(ledState ? "Menyala" : "Mati");
+        } else {
+            Serial.println("-> Perintah tidak dikenal!");
+        }
+    }
+
+    delay(100);
+}
